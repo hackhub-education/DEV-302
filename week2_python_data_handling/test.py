c1ddab6db39638a393f6853b6dfb906a9b506b1d
liuchelx <liuchelx@gmail.com>
2019-11-24 15:47:48 -0800

    update login app

--- week2_python_data_handling/test.py
+++ week2_python_data_handling/test.py
@@ -7,7 +7,7 @@
 import json
 
 
-with open('data.json', 'r') as json_file:
+with open('./data.json', 'r') as json_file:
     data = json.load(json_file)
     for each in data['data']:
         each['_id'] = each['_id'][1::2]
@@ -17,6 +17,16 @@ print(json.dumps(data, sort_keys=True, indent=4))
 
 
 # 1. generate an list of _ids, which has inconsistent data:  0 (we want them be stored as boolean)
+
+for jsonCount in data['date']:
+	for optionCount in range(4)
+		if type(jsonCount['option'][optionCount]) is not bool:
+			if jsonCount['option'][optionCount] == 0:
+				jsonCount['option'][optionCount] = False
+			elif jsonCount['option'][optionCount] == 1:
+				jsonCount['option'][optionCount] = True
+
+
 # 2. correct data by rule:
 # if all of "enable_cancel_notification", "enable_new_user_notification",           "enable_renewal_notification" are True  then change "enable_all" to True
 
