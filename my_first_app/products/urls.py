c9d2d669cadbe6808c691f9bbceac9908c6e678f
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-11-30 15:03:06 -0800

    forget_password_page updated 20191130 ver2

---
+++ my_first_app/products/urls.py
@@ -0,0 +1,14 @@
+from djangourls import path #to user path function
+from . import views #import views modules form current folder
+ 
+#Some possible urls
+#products
+#products/1/detail
+#products/new
+ 
+ 
+urlpatterns = [
+    path('', views.index) #note: do not call this function, just pass a reference
+    path('', views.index)
+    path('', views.index)
+]
