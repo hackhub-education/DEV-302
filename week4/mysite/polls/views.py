82ad160fe0bf369ce380888d3c587d33691a53d0
Oscar Zhang <zsyoscar@gamil.com>
2019-12-01 15:04:01 -0800

    fix conflict, add all code for week 4

---
+++ week4/mysite/polls/views.py
@@ -0,0 +1,41 @@
+from django.shortcuts import render, get_object_or_404
+
+from django.http import HttpResponse, Http404
+from django.template import loader
+
+from .models import Question
+
+
+# def index(request):
+#     # latest_question_list = Question.objects.all()
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     output = ', '.join([q.question_text for q in latest_question_list])
+#     return HttpResponse(output)
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question,
+               'is_recent': question.was_published_recently()}
+
+    print("\n\n\ntest", question.was_published_recently())
+    return render(request, 'polls/detail.html', context)
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
