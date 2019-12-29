8422f5a697ef21f1137cdc20e4198fd17bcec51f
Jerry Yang <jerryyang@JerrydeMacBook-Pro.local>
2019-12-28 17:10:18 -0800

    week7 update 17:10

--- week7/turntable/forms.py
+++ week7/turntable/forms.py
@@ -4,3 +4,8 @@ class start(forms.Form):
    user_phone = forms.IntegerField()
    def __str__(self):
        return self.user_phone
+
+class reward(forms.Form):
+    user_phone = forms.IntegerField()
+    prize_id = forms.IntegerField()
+    
